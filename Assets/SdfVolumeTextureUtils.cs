fa74ee3c20fea9ce2d6cdb25ceaf89e74046ea1b
Mike Rotondo <mrotondo@codeforamerica.org>
2024-07-05 14:47:30 -0500

    switch sphere blitting to use transforms

--- Assets/SdfVolumeTextureUtils.cs
+++ Assets/SdfVolumeTextureUtils.cs
@@ -1,3 +1,5 @@
+using System;
+using TreeEditor;
 using Unity.Collections;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -14,9 +16,9 @@ public class SdfVolumeTextureUtils
 
         InitSdfVolumeTexture(texture, 1.0f);
 
-        BlitSphereToSdfVolumeTexture(texture, new Vector3(0.5f, 0.5f, 0.5f), 0.25f);
-        BlitSphereToSdfVolumeTexture(texture, new Vector3(0.7f, 0.7f, 0.7f), 0.2f);
-        BlitSphereToSdfVolumeTexture(texture, new Vector3(0.2f, 0.2f, 0.2f), 0.1f);
+        BlitSphereToSdfVolumeTexture(texture, Matrix4x4.TRS(Vector3.one * 0.5f, Quaternion.identity, Vector3.one * 0.5f));
+        BlitSphereToSdfVolumeTexture(texture, Matrix4x4.TRS(Vector3.one * 0.7f, Quaternion.identity, Vector3.one * 0.4f));
+        BlitSphereToSdfVolumeTexture(texture, Matrix4x4.TRS(Vector3.one * 0.2f, Quaternion.identity, Vector3.one * 0.2f));
 
         return texture;
     }
@@ -24,7 +26,7 @@ public class SdfVolumeTextureUtils
     private static void InitSdfVolumeTexture(Texture3D sdfVolumeTexture, float initDistance)
     {
         byte[] data = new byte[sdfVolumeTexture.width * sdfVolumeTexture.height * sdfVolumeTexture.depth];
-        
+
         for (int z = 0; z < sdfVolumeTexture.depth; z++)
         {
             int zOffset = z * sdfVolumeTexture.height * sdfVolumeTexture.width;
@@ -42,7 +44,7 @@ public class SdfVolumeTextureUtils
     }
 
     // Naive implementation! Could be improved by breaking the texture up into cells and only updating cells that overlap with the sphere
-    public static void BlitSphereToSdfVolumeTexture(Texture3D sdfVolumeTexture, Vector3 sphereCenter, float sphereRadius)
+    public static void BlitSphereToSdfVolumeTexture(Texture3D sdfVolumeTexture, Matrix4x4 sphereTrs)
     {
         NativeArray<byte> data = sdfVolumeTexture.GetPixelData<byte>(0);
 
@@ -56,11 +58,14 @@ public class SdfVolumeTextureUtils
                 {
                     // This only works because we're a unit cube with our lower front left at 0,0,0
                     // We'll need a smarter transform later to get cell position
-                    Vector3 position = new(
+                    Vector3 samplePosition = new(
                         x / (sdfVolumeTexture.width - 1.0f),
                         y / (sdfVolumeTexture.height - 1.0f),
                         z / (sdfVolumeTexture.depth - 1.0f));
-                    float sphereDistance = Vector3.Distance(position, sphereCenter) - sphereRadius;
+
+                    Vector3 samplePositionInObjectSpace = WorldToObjectSpace(samplePosition, sphereTrs);
+                    float unscaledSphereDistance = SphereDistance(samplePositionInObjectSpace, 0.5f);
+                    float sphereDistance = SafeScaleFactor(sphereTrs) * unscaledSphereDistance;
 
                     float oldDistance = ByteToDistance(data[x + yOffset + zOffset]);
                     if (sphereDistance < oldDistance)
@@ -72,6 +77,26 @@ public class SdfVolumeTextureUtils
         }
         sdfVolumeTexture.SetPixelData<byte>(data, 0);
         sdfVolumeTexture.Apply();
+    }
+
+    private static Vector4 WorldToObjectSpace(Vector3 position, Matrix4x4 objectTrs)
+    {
+        Vector4 homogeneousPoint = position;
+        homogeneousPoint.w = 1;
+        return objectTrs.inverse * homogeneousPoint;
+    }
+
+    private static float SafeScaleFactor(Matrix4x4 objectTrs)
+    {
+        Vector3 x = objectTrs * Vector3.right;
+        Vector3 y = objectTrs * Vector3.up;
+        Vector3 z = objectTrs * Vector3.forward;
+        return Math.Min(x.magnitude, Math.Min(y.magnitude, z.magnitude));
+    }
+
+    private static float SphereDistance(Vector3 samplePoint, float radius)
+    {
+        return samplePoint.magnitude - radius;
     }
 
     private static byte DistanceToByte(float distance)
